f6d950f4061a3a756716ffe9d7149314c4cf53c9
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-01-08 11:40:14 +0100

    creating an audience

--- app.js
+++ app.js
@@ -12,9 +12,71 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body);
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const useremail = req.body.email;
+    dc="us14";
+    apikey="8a8c6cb060980945bf7bbac525caec55-us14"
+
+    // curl -sS \
+    // "https://${dc}.api.mailchimp.com/3.0/ping" \
+    // --user "anystring:${apikey}" | jq -r
+
+    // axios.get(`https://${dc}.api.mailchimp.com/3.0/ping`,{
+    //     auth: {
+    //         username: 'umole',
+    //         password: apikey,
+    //         //user: `umole:${apikey}`
+    //     }
+    // }).then((response) => {
+    //     console.log(response.data);
+    // }).catch((error) => {
+    //     console.error(error);
+    // });
+
+
+    const event_name = "JavaScript Developers Meetup";
+
+    const footer_contact_info = {
+    company: "Mailchimp",
+    address1: "675 Ponce de Leon Ave NE",
+    address2: "Suite 5000",
+    city: "Atlanta",
+    state: "GA",
+    zip: "30308",
+    country: "US"
+    }
+
+    const campaign_defaults = {
+    from_name: "Gettin'\'' Together",
+    from_email: "gettintogether@example.com",
+    subject: "Bash Developers Meetup",
+    language: "EN_US"
+    }
+
+    axios.post(`https://${dc}.api.mailchimp.com/3.0/lists`, {
+        name: event_name,
+        contact: footer_contact_info,
+        permission_reminder: "permission_reminder",
+        email_type_option: true,
+        campaign_defaults: campaign_defaults
+    }, {
+        auth: {
+            username: 'umole',
+            password: apikey,
+          }
+    }).then((response) => {
+        console.log(response.body);
+    }).catch((error) => {
+        console.error(error);
+    });
+
 });
 
 app.listen(3000, () => {
     console.log("Server is listening on PORT: 3000");
 });
+
+// API KEY
+// 8a8c6cb060980945bf7bbac525caec55
+
