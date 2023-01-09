d35e94377aa5ced2aa484f419f44bb51030ede2e
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-01-09 22:00:48 +0100

    using sendgrip api

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+const { response } = require('express');
+require('dotenv').config();
+
 
 const app = express();
 
@@ -15,61 +19,27 @@ app.post('/', (req, res) => {
     const firstname = req.body.firstname;
     const lastname = req.body.lastname;
     const useremail = req.body.email;
-    dc="us14";
-    apikey="8a8c6cb060980945bf7bbac525caec55-us14"
 
-    // curl -sS \
-    // "https://${dc}.api.mailchimp.com/3.0/ping" \
-    // --user "anystring:${apikey}" | jq -r
-
-    // axios.get(`https://${dc}.api.mailchimp.com/3.0/ping`,{
-    //     auth: {
-    //         username: 'umole',
-    //         password: apikey,
-    //         //user: `umole:${apikey}`
-    //     }
-    // }).then((response) => {
-    //     console.log(response.data);
-    // }).catch((error) => {
-    //     console.error(error);
-    // });
-
-
-    const event_name = "JavaScript Developers Meetup";
-
-    const footer_contact_info = {
-    company: "Mailchimp",
-    address1: "675 Ponce de Leon Ave NE",
-    address2: "Suite 5000",
-    city: "Atlanta",
-    state: "GA",
-    zip: "30308",
-    country: "US"
-    }
-
-    const campaign_defaults = {
-    from_name: "Gettin'\'' Together",
-    from_email: "gettintogether@example.com",
-    subject: "Bash Developers Meetup",
-    language: "EN_US"
-    }
-
-    axios.post(`https://${dc}.api.mailchimp.com/3.0/lists`, {
-        name: event_name,
-        contact: footer_contact_info,
-        permission_reminder: "permission_reminder",
-        email_type_option: true,
-        campaign_defaults: campaign_defaults
-    }, {
-        auth: {
-            username: 'umole',
-            password: apikey,
-          }
-    }).then((response) => {
-        console.log(response.body);
-    }).catch((error) => {
-        console.error(error);
-    });
+    // using Twilio SendGrid's v3 Node.js Library
+    // https://github.com/sendgrid/sendgrid-nodejs
+    //javascript
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+    to: 'umolecodes@gmail.com', // Change to your recipient
+    from: 'ozimedeumole@gmail.com', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    } 
+    sgMail
+    .send(msg)
+    .then(() => {
+        console.log('Email sent')
+    })
+    .catch((error) => {
+        console.error(error)
+    })
 
 });
 
